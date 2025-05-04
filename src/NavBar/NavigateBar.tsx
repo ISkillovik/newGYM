cfd26b7b3e93dcf7ab6eb167486351be5e033c8e
Skillovik <arambadasyan93@gmail.com>
2025-05-04 21:20:08 +0400

    v1.02

--- src/NavBar/NavigateBar.tsx
+++ src/NavBar/NavigateBar.tsx
@@ -1,7 +1,52 @@
+import { Link } from "react-router-dom";
+import styles from "./NavMenu.module.css";
+import { useState } from "react";
+
 interface Props {}
 
 const NavigateBar = (props: Props) => {
-  return <div>NavigateBar</div>;
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  return (
+    <nav className={styles.NavCountain}>
+      <Link to={"/"} className={styles.NavLogo}>
+        <img
+          src={require("../../icons/gym-logo.png")}
+          alt=""
+          className={styles.logo}
+        />
+        <p>GymAsist</p>
+      </Link>
+      <label className={styles.hamburgerMenu}>
+        <input
+          onChange={() => {
+            setMenuOpen(!menuOpen);
+          }}
+          type="checkbox"
+        />
+      </label>
+
+      <ul className={menuOpen ? styles.OpenMenu : ""}>
+        {/* {userName ? (
+      <li>
+        <Link>{userName}</Link>
+      </li>
+    ) : (
+      ""
+    )} */}
+
+        <li>{/* <Link>About</Link> */}</li>
+        <li>
+          <Link to={"/progress"}>My Progress</Link>
+        </li>
+        <li>{/* <Link>Settings</Link> */}</li>
+        <li onClick={() => {}}>
+          <Link to={"/"} className={styles.NavLogo}>
+            Log out
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavigateBar;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,15 @@
+import { Outlet } from "react-router-dom";
+import NavigateBar from "../NavBar/NavigateBar";
+
 interface Props {}
 
 const MainLayout = (props: Props) => {
-  return <div>MainLayout</div>;
+  return (
+    <>
+      <NavigateBar />
+      <Outlet />
+    </>
+  );
 };
 
 export default MainLayout;
